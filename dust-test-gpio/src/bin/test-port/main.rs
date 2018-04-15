2f754c54c4e0fa1775e4f76876e08b4604e7ae51
Juergen Stuber <juergen@jstuber.net>
2018-04-15 17:30:34 +0200

    Add crate dust-test-gpio.

---
+++ dust-test-gpio/src/bin/test-port/main.rs
@@ -0,0 +1,129 @@
+#![feature(asm)]
+#![feature(const_fn)]
+#![feature(lang_items)]
+#![feature(linkage)]
+#![feature(naked_functions)]
+#![feature(used)]
+#![no_std]
+#![no_main]
+
+extern crate dust;
+#[cfg(feature = "lpc11xx")]
+extern crate dust_lpc11xx;
+#[cfg(feature = "lpc13xx")]
+extern crate dust_lpc13xx;
+#[cfg(feature = "lpc8xx")]
+extern crate dust_lpc8xx;
+
+#[cfg(all(feature = "lpc8xx", not(feature = "lpc81x")))]
+use dust::gpio::port::DirSet;
+#[cfg(any(feature = "lpc81x", feature = "lpc11xx", feature = "lpc13xx"))]
+use dust::gpio::port::DirSetValue;
+use dust::gpio::port::{Clr, Set};
+
+#[cfg(feature = "lpc8xx")]
+use dust_lpc8xx::GPIO;
+#[cfg(feature = "lpc8xx")]
+use dust_lpc8xx::gpio;
+
+#[cfg(feature = "lpc11xx")]
+use dust_lpc11xx::GPIO;
+#[cfg(feature = "lpc11xx")]
+use dust_lpc11xx::gpio;
+
+#[cfg(feature = "lpc13xx")]
+use dust_lpc13xx::GPIO;
+#[cfg(feature = "lpc13xx")]
+use dust_lpc13xx::gpio;
+
+fn delay(n: usize) {
+    for _ in 0..n {
+        // Make sure the loop is not optimized away
+        unsafe { asm!("" :::: "volatile") }
+    }
+}
+
+#[cfg(any(feature = "lpc802", feature = "lpc804"))]
+const LED: (usize, usize) = (0, 15);
+#[cfg(feature = "lpc810")]
+const LED: (usize, usize) = (0, 1);
+#[cfg(any(feature = "lpc812", feature = "lpc824"))]
+const LED: (usize, usize) = (0, 15);
+#[cfg(feature = "lpc845")]
+const LED: (usize, usize) = (0, 0);
+#[cfg(feature = "lpc1114")]
+const LED: (usize, usize) = (0, 9);
+#[cfg(feature = "lpc1343")]
+const LED: (usize, usize) = (3, 0);
+
+#[cfg(any(feature = "lpc802", feature = "lpc804"))]
+fn enable_gpio_clock() {
+    use dust_lpc8xx::syscon;
+    use dust_lpc8xx::SYSCON;
+    unsafe {
+        let syscon = &mut *SYSCON;
+        syscon.enable_clock(syscon::CLOCK_GPIO0);
+    }
+}
+
+#[cfg(feature = "lpc84x")]
+fn enable_gpio_clock() {
+    use dust_lpc8xx::syscon;
+    use dust_lpc8xx::SYSCON;
+    unsafe {
+        let syscon = &mut *SYSCON;
+        syscon.enable_clock(syscon::CLOCK_GPIO0);
+        syscon.enable_clock(syscon::CLOCK_GPIO1);
+    }
+}
+
+#[cfg(not(any(feature = "lpc802", feature = "lpc804", feature = "lpc84x")))]
+fn enable_gpio_clock() {
+    // GPIO clock is already enabled after reset
+}
+
+
+#[cfg(feature = "lpc81x")]
+fn init_gpio_port(port: &mut gpio::Port, bit_index: usize) {
+    port.dir_modify_value(|w| w | (1 << bit_index));
+}
+
+#[cfg(all(feature = "lpc8xx", not(feature = "lpc81x")))]
+fn init_gpio_port(port: &mut gpio::Port, bit_index: usize) {
+    port.dir_set(1 << bit_index);
+}
+
+#[cfg(feature = "lpc8xx")]
+fn get_gpio_port() -> gpio::Port<'static> {
+    let gpio = unsafe { &mut *GPIO };
+    let (port_index, bit_index) = LED;
+    let mut port = unsafe { gpio::Port::new(gpio, port_index) };
+    init_gpio_port(&mut port, bit_index);
+    port
+}
+
+#[cfg(any(feature = "lpc11xx", feature = "lpc13xx"))]
+fn get_gpio_port() -> gpio::Port<'static> {
+    let (port_index, bit_index) = LED;
+    let gpio = unsafe { &mut *GPIO[port_index] };
+    let mut port = unsafe { gpio::Port::new(gpio) };
+    port.dir_modify_value(|w| w | (1 << bit_index));
+    port
+}
+
+#[no_mangle]
+pub fn main() {
+    enable_gpio_clock();
+    let mut gpio_port = get_gpio_port();
+    let d = 500000;
+    let n = 2;
+    loop {
+        for _ in 0..n {
+            let (_, bit_index) = LED;
+            gpio_port.set_bits(1 << bit_index);
+            delay(d);
+            gpio_port.clr_bits(1 << bit_index);
+            delay(d);
+        }
+    }
+}
