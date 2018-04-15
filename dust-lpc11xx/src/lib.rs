5a550096aa436ac9f4e1fa4fe33e9ffb78c09736
Juergen Stuber <juergen@jstuber.net>
2018-04-15 17:35:23 +0200

    Update dust-lpc11xx.

--- dust-lpc11xx/src/lib.rs
+++ dust-lpc11xx/src/lib.rs
@@ -3,24 +3,28 @@
 #![feature(used)]
 #![no_std]
 
+#[macro_use]
+extern crate dust;
 extern crate dust_cortex_m;
 extern crate volatile_register;
 
 #[cfg(feature = "lpc11xx")]
 pub mod lpc11xx_vectors;
 
-pub mod syscon;
-pub mod iocon;
 pub mod gpio;
+pub mod iocon;
+pub mod syscon;
 pub mod uart;
 
 pub const SYSCON: *mut syscon::Syscon = 0x4004_8000 as *mut syscon::Syscon;
 
 pub const IOCON: *mut iocon::Iocon = 0x4004_4000 as *mut iocon::Iocon;
 
-pub const GPIO0: *mut gpio::Gpio = 0x5000_0000 as *mut gpio::Gpio;
-pub const GPIO1: *mut gpio::Gpio = 0x5001_0000 as *mut gpio::Gpio;
-pub const GPIO2: *mut gpio::Gpio = 0x5002_0000 as *mut gpio::Gpio;
-pub const GPIO3: *mut gpio::Gpio = 0x5003_0000 as *mut gpio::Gpio;
+pub const GPIO: [*mut gpio::Gpio; 4] = [
+    0x5000_0000 as *mut gpio::Gpio,
+    0x5001_0000 as *mut gpio::Gpio,
+    0x5002_0000 as *mut gpio::Gpio,
+    0x5003_0000 as *mut gpio::Gpio,
+];
 
 pub const UART: *mut uart::Uart = 0x4000_8000 as *mut uart::Uart;
--- dust-lpc11xx/src/lpc11xx_vectors.rs
+++ dust-lpc11xx/src/lpc11xx_vectors.rs
@@ -1,181 +1,76 @@
+default_handler!(pio0_0_irq_handler);
+default_handler!(pio0_1_irq_handler);
+default_handler!(pio0_2_irq_handler);
+default_handler!(pio0_3_irq_handler);
+default_handler!(pio0_4_irq_handler);
+default_handler!(pio0_5_irq_handler);
+default_handler!(pio0_6_irq_handler);
+default_handler!(pio0_7_irq_handler);
+default_handler!(pio0_8_irq_handler);
+default_handler!(pio0_9_irq_handler);
+default_handler!(pio0_10_irq_handler);
+default_handler!(pio0_11_irq_handler);
+default_handler!(pio1_0_irq_handler);
+default_handler!(c_can_irq_handler);
+default_handler!(spi1_irq_handler);
+default_handler!(i2c_irq_handler);
+default_handler!(ct16b0_irq_handler);
+default_handler!(ct16b1_irq_handler);
+default_handler!(ct32b0_irq_handler);
+default_handler!(ct32b1_irq_handler);
+default_handler!(spi0_irq_handler);
+default_handler!(uart_irq_handler);
+default_handler!(adc_irq_handler);
+default_handler!(wdt_irq_handler);
+default_handler!(bod_irq_handler);
+default_handler!(gpio3_irq_handler);
+default_handler!(gpio2_irq_handler);
+default_handler!(gpio1_irq_handler);
+default_handler!(gpio0_irq_handler);
+
+
 #[used]
 #[link_section = ".irq_vectors"]
 pub static IRQ_VECTORS: [Option<fn()>; 32] = [
-    Some(pio0_0_irq_handler), //  0
+    // 0
+    Some(pio0_0_irq_handler),
     Some(pio0_1_irq_handler),
     Some(pio0_2_irq_handler),
     Some(pio0_3_irq_handler),
+    // 4
     Some(pio0_4_irq_handler),
-    Some(pio0_5_irq_handler), //  5
+    Some(pio0_5_irq_handler),
     Some(pio0_6_irq_handler),
     Some(pio0_7_irq_handler),
+    // 8
     Some(pio0_8_irq_handler),
     Some(pio0_9_irq_handler),
-    Some(pio0_10_irq_handler), // 10
+    Some(pio0_10_irq_handler),
     Some(pio0_11_irq_handler),
+    // 12
     Some(pio1_0_irq_handler),
     Some(c_can_irq_handler),
     Some(spi1_irq_handler),
-    Some(i2c_irq_handler), // 15
+    Some(i2c_irq_handler),
+    // 16
     Some(ct16b0_irq_handler),
     Some(ct16b1_irq_handler),
     Some(ct32b0_irq_handler),
     Some(ct32b1_irq_handler),
-    Some(spi0_irq_handler), // 20
+    // 20
+    Some(spi0_irq_handler),
     Some(uart_irq_handler),
     None,
     None,
+    // 24
     Some(adc_irq_handler),
-    Some(wdt_irq_handler), // 25
+    Some(wdt_irq_handler),
     Some(bod_irq_handler),
     None,
+    // 28
     Some(gpio3_irq_handler),
     Some(gpio2_irq_handler),
-    Some(gpio1_irq_handler), // 30
+    Some(gpio1_irq_handler),
     Some(gpio0_irq_handler),
+    // 32
 ];
-
-#[linkage = "weak"]
-pub fn pio0_0_irq_handler() {
-    loop {}
-}
-
-#[linkage = "weak"]
-pub fn pio0_1_irq_handler() {
-    loop {}
-}
-
-#[linkage = "weak"]
-pub fn pio0_2_irq_handler() {
-    loop {}
-}
-
-#[linkage = "weak"]
-pub fn pio0_3_irq_handler() {
-    loop {}
-}
-
-#[linkage = "weak"]
-pub fn pio0_4_irq_handler() {
-    loop {}
-}
-
-#[linkage = "weak"]
-pub fn pio0_5_irq_handler() {
-    loop {}
-}
-
-#[linkage = "weak"]
-pub fn pio0_6_irq_handler() {
-    loop {}
-}
-
-#[linkage = "weak"]
-pub fn pio0_7_irq_handler() {
-    loop {}
-}
-
-#[linkage = "weak"]
-pub fn pio0_8_irq_handler() {
-    loop {}
-}
-
-#[linkage = "weak"]
-pub fn pio0_9_irq_handler() {
-    loop {}
-}
-
-#[linkage = "weak"]
-pub fn pio0_10_irq_handler() {
-    loop {}
-}
-
-#[linkage = "weak"]
-pub fn pio0_11_irq_handler() {
-    loop {}
-}
-
-#[linkage = "weak"]
-pub fn pio1_0_irq_handler() {
-    loop {}
-}
-
-#[linkage = "weak"]
-pub fn c_can_irq_handler() {
-    loop {}
-}
-
-#[linkage = "weak"]
-pub fn spi1_irq_handler() {
-    loop {}
-}
-
-#[linkage = "weak"]
-pub fn i2c_irq_handler() {
-    loop {}
-}
-
-#[linkage = "weak"]
-pub fn ct16b0_irq_handler() {
-    loop {}
-}
-
-#[linkage = "weak"]
-pub fn ct16b1_irq_handler() {
-    loop {}
-}
-
-#[linkage = "weak"]
-pub fn ct32b0_irq_handler() {
-    loop {}
-}
-
-#[linkage = "weak"]
-pub fn ct32b1_irq_handler() {
-    loop {}
-}
-
-#[linkage = "weak"]
-pub fn spi0_irq_handler() {
-    loop {}
-}
-
-#[linkage = "weak"]
-pub fn uart_irq_handler() {
-    loop {}
-}
-
-#[linkage = "weak"]
-pub fn adc_irq_handler() {
-    loop {}
-}
-
-#[linkage = "weak"]
-pub fn wdt_irq_handler() {
-    loop {}
-}
-
-#[linkage = "weak"]
-pub fn bod_irq_handler() {
-    loop {}
-}
-
-#[linkage = "weak"]
-pub fn gpio3_irq_handler() {
-    loop {}
-}
-
-#[linkage = "weak"]
-pub fn gpio2_irq_handler() {
-    loop {}
-}
-
-#[linkage = "weak"]
-pub fn gpio1_irq_handler() {
-    loop {}
-}
-
-#[linkage = "weak"]
-pub fn gpio0_irq_handler() {
-    loop {}
-}
